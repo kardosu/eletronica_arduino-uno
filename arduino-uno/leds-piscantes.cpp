4d3ea72cd6e2f4f49df2087e6ff4f588de5049d3
kardosu <126108212+kardosu@users.noreply.github.com>
2024-02-15 15:03:39 -0300

    Código C++

--- arduino-uno/leds-piscantes.cpp
+++ arduino-uno/leds-piscantes.cpp
@@ -1,1 +1,18 @@
+// C++ code
 
+int led_pin1 = 12;
+int led_pin2 = 13;
+
+void setup() {
+  pinMode (led_pin1, OUTPUT);
+  pinMode (led_pin2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led_pin1, HIGH);
+  digitalWrite(led_pin2, HIGH);
+  delay(1000);
+  digitalWrite(led_pin1, LOW);
+  digitalWrite(led_pin2, LOW);
+  delay(150);
+}
